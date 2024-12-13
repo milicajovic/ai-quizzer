9a64a537986e2b5f455c25d5f94fb703f05e71e4
Milica Jovic <11442650+milicajovic@users.noreply.github.com>
2024-12-13 15:03:31 +0100

    Port number changer to 8181

--- run.py
+++ run.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     # with app.app_context():
     #     db.create_all()
     #app.run()
-    app.run(port=8000, host='0.0.0.0', debug=True)
+    app.run(port=8181, host='0.0.0.0', debug=True)
