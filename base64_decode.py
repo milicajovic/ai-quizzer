1c248ddb5d9486d91956f6a028511f21c53c8648
Milica Jovic <11442650+milicajovic@users.noreply.github.com>
2024-12-26 19:16:03 +0100

    google app credentials bug fixed

--- base64_decode.py
+++ base64_decode.py
@@ -3,15 +3,15 @@ import base64
 import tempfile
 
 def setup_google_credentials():
-    BASE64_KEY = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_BASE64")
+    GOOGLE_APPLICATION_CREDENTIALS_BASE64 = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_BASE64")
 
-    if BASE64_KEY:
-        json_bytes = base64.b64decode(BASE64_KEY)
+    if GOOGLE_APPLICATION_CREDENTIALS_BASE64:
+        json_bytes = base64.b64decode(GOOGLE_APPLICATION_CREDENTIALS_BASE64)
 
         with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp_file:
             temp_file.write(json_bytes)
             temp_file_path = temp_file.name
 
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = temp_file_path
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS_BASE64"] = temp_file_path
     else:
         raise ValueError("GOOGLE_APPLICATION_CREDENTIALS_BASE64 is not set")
